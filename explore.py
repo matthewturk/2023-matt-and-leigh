5abf5d022468c59ae2045ca586e2e4e6b3101db2
Leigh Orf <leigh.orf@ssec.wisc.edu>
2023-02-23 14:44:21 -0600

    Messing around with plots. Trying to turn on bicubic interpolation, no luck thus far. Following example here: https://yt-project.org/doc/cookbook/complex_plots.html ("varying resolution...")

--- explore.py
+++ explore.py
@@ -1,7 +1,22 @@
 import hdf5plugin
 import yt
 
-ds = yt.load("for_Matt_Turk/mkow075-ens-db620.00790000.nc")
+ds = yt.load("/Dropbox-nvme/orf/Dropbox/research/2022/projects/yt/for_Matt_Turk/mkow075-ens-db620.00790000.nc")
 ds.parameters
-s = ds.r[:,:,10.0].plot("vortmag")
-s.save()
+
+#s = ds.r[:,:,0.0376].plot("vortmag")
+s = ds.r[:,0.0,:].plot("vortmag")
+s.set_cmap(("vortmag"),"nipy_spectral")
+s.set_log(("vortmag"),False)
+s.set_zlim(("vortmag"),zmin=(4e-3,"1/s"),zmax=(0.2,"1/s"))
+s.set_buff_size(2000)
+s.set_figure_size(10)
+print(list(s.plots)[0])
+plot = s.plots[list(s.plots)[0]]
+print(plot)
+ax=plot.axes
+img=ax.images[0]
+print(img)
+#following does nothing to smooth plot (seems to be pixel based, no interpolation)
+img.set_interpolation("bicubic")
+s.save("full.png",mpl_kwargs=dict(dpi=300))
